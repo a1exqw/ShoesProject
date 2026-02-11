948febdd5685f5e7632c5da6d1f757ee951ce7f3
a1exqw <nagaev1117@mail.ru>
2026-02-11 09:45:14 +0500

    Добавление функционала FormLogin

--- ShoesProject/FormLogin.Designer.cs
+++ ShoesProject/FormLogin.Designer.cs
@@ -110,6 +110,7 @@
             btnGuest.TabIndex = 5;
             btnGuest.Text = "Войти как гость";
             btnGuest.UseVisualStyleBackColor = false;
+            btnGuest.Click += BtnGuest_Click;
             // 
             // btnLogin
             // 
@@ -123,6 +124,7 @@
             btnLogin.TabIndex = 4;
             btnLogin.Text = "Войти";
             btnLogin.UseVisualStyleBackColor = false;
+            btnLogin.Click += BtnLogin_Click;
             // 
             // FormLogin
             // 
